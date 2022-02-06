4ae20d336b26af41fefa817f78ea3f7629245d4e
Eyup Ersoy <eersoy84@gmail.com>
2022-02-07 00:25:25 +0300

    final gece 3

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
 export let API_BASE = "";
 if (process.env.NODE_ENV === 'production') {
-    // API_BASE = `${api.bizleal.net}/api`
-    API_BASE = "https://api.bizleal.net/api"
-    console.log("API_BASE2=>", process.env.REACT_APP_PROD_API)
+    API_BASE = `${process.env.NEXT_PUBLIC_PROD_URL}/api`
+    // API_BASE = "https://api.bizleal.net/api"
+    console.log("API_BASE2=>", process.env.NEXT_PUBLIC_PROD_URL)
 } else {
     API_BASE = `${process.env.NEXT_PUBLIC_DEV_URL}/api`
 }
